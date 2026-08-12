b7a4c39968f76aa3523756ae711da1e6e4e2d53c
Davi Souza <65422945+WolfSondar@users.noreply.github.com>
2026-08-11 21:29:13 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Cole aqui a URL do seu Apps Script (a que termina em /exec).
+// Veja o passo a passo no arquivo Code.gs ou no README.md.
+const API_URL = "https://script.google.com/macros/s/AKfycbwWjdumDVqvrrPAkJw8QIG1zpWSN38wgD-clNrEeHk26pvZpwbztkKQ1Y6Lql1vuC2g_g/exec";
