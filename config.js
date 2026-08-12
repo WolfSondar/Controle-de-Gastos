6969a546fcad5a9eb85b2fcdfb32ac07e9ffb70f
Davi Souza <65422945+WolfSondar@users.noreply.github.com>
2026-08-11 21:28:43 -0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Cole aqui a URL do seu Apps Script (a que termina em /exec).
-// Veja o passo a passo no arquivo Code.gs ou no README.md.
-const API_URL = "https://script.google.com/macros/s/AKfycbyyO7Yl7CjsoexrPnqamEBpUFEIOD0HIg70g7-Rcmp1LQtYcSyDdiZ8_wDVCHvdt9152A/exec";
