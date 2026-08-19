271a79acd7855a94b57692e89ab6e3278fbef074
Davi Souza <65422945+WolfSondar@users.noreply.github.com>
2026-08-19 08:05:22 -0300

    Upgrade CACHE_VERSION from caixa-v4 to caixa-v5

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 // perceber que precisa baixar os arquivos novos de novo.
 // =====================================================================
 
-const CACHE_VERSION = "caixa-v4";
+const CACHE_VERSION = "caixa-v5";
 const CACHE_SHELL = `${CACHE_VERSION}-shell`;
 const CACHE_RUNTIME = `${CACHE_VERSION}-runtime`;
 
@@ -24,6 +24,7 @@ const APP_SHELL = [
   "./",
   "./index.html",
   "./style.css",
+  "./melhorias.css",
   "./app.js",
   "./config.js",
   "./manifest.json",
