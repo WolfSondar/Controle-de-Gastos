63254135a1d121ba9d1ae5f6f21270466a04cdd9
Davi Souza <65422945+WolfSondar@users.noreply.github.com>
2026-08-21 08:43:12 -0300

    Update CACHE_VERSION to caixa-v12

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 // perceber que precisa baixar os arquivos novos de novo.
 // =====================================================================
 
-const CACHE_VERSION = "caixa-v11";
+const CACHE_VERSION = "caixa-v12";
 const CACHE_SHELL = `${CACHE_VERSION}-shell`;
 const CACHE_RUNTIME = `${CACHE_VERSION}-runtime`;
 
