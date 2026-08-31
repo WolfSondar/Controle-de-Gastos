42f66ac2e6f33a75c89b83799e1d8b0034b5a781
Davi Souza <65422945+WolfSondar@users.noreply.github.com>
2026-08-31 17:19:27 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 // perceber que precisa baixar os arquivos novos de novo.
 // =====================================================================
 
-const CACHE_VERSION = "caixa-v19";
+const CACHE_VERSION = "caixa-v20";
 const CACHE_SHELL = `${CACHE_VERSION}-shell`;
 const CACHE_RUNTIME = `${CACHE_VERSION}-runtime`;
 
