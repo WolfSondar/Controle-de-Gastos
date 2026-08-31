569b08ecd775723998e845450d856108e17b0fd5
Davi Souza <65422945+WolfSondar@users.noreply.github.com>
2026-08-31 11:13:02 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Cole aqui a URL do seu Apps Script (a que termina em /exec).
 // Veja o passo a passo no arquivo Code.gs ou no README.md.
-const API_URL = "https://script.google.com/macros/s/AKfycbw_wPDVO8NN6oZEdnX-NGJ3wDbfplcKaVSJFncYwEfHswEND9uT28VJGelZoHxYjorJFg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxnbpEZUbAyMYOSLqGKWiluJ5bZSViqmNxm6rRWGtzFDfaVf54j5PPnTrMdyijuiNgBww/exec";
