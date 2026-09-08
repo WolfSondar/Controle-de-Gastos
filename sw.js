b4d00d726dba2fff846be40805e23a81c24ca7ca
Davi Souza <65422945+WolfSondar@users.noreply.github.com>
2026-09-08 17:41:19 -0300

    Update CACHE_VERSION to caixa-v32

--- sw.js
+++ sw.js
@@ -12,7 +12,7 @@
 // perceber que precisa baixar os arquivos novos de novo.
 // =====================================================================
 
-const CACHE_VERSION = "caixa-v31";
+const CACHE_VERSION = "caixa-v32";
 const CACHE_SHELL = `${CACHE_VERSION}-shell`;
 const CACHE_RUNTIME = `${CACHE_VERSION}-runtime`;
 
