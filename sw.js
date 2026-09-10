37076fbbe30501352ad30a0d95d53e782863c312
Davi Souza <65422945+WolfSondar@users.noreply.github.com>
2026-09-10 17:01:57 -0300

    Update CACHE_VERSION to caixa-v44

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 // Apps Script e não são armazenados pelo Service Worker.
 // =====================================================================
 
-const CACHE_VERSION = "caixa-v43";
+const CACHE_VERSION = "caixa-v44";
 const CACHE_SHELL = `${CACHE_VERSION}-shell`;
 const CACHE_RUNTIME = `${CACHE_VERSION}-runtime`;
 
