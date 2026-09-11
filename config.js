5b45842d0f55862943193023e5c13decb3c0161f
Davi Souza <65422945+WolfSondar@users.noreply.github.com>
2026-09-10 22:03:19 -0300

    Update API_URL to new Google Apps Script URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Cole aqui a URL do seu Apps Script (a que termina em /exec).
 // Veja o passo a passo no arquivo Code.gs ou no README.md.
-const API_URL = "https://script.google.com/macros/s/AKfycbyzNFr1YM5jjDFLa5fFLJ2FXWO3YHphDvQK7OOk_zKVS0ioUpJzbrTsauVOHOTjp5txyw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzwX3bhQs9TG5bz8WWgWoUzkROOKu5dtQUGIuBVpANW1s6QIO_LjrNuaNo12qDjoTY5gA/exec";
