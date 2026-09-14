55c1b2c3613f1cb360de9007f4e13fcf2ad4750c
Davi Souza <65422945+WolfSondar@users.noreply.github.com>
2026-09-14 17:23:53 -0300

    Update Firebase configuration credentials
    Updated Firebase configuration with new credentials.

--- firebase-config.js
+++ firebase-config.js
@@ -8,10 +8,11 @@
 // ============================================================
 
 window.CAIXA_FIREBASE_CONFIG = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  storageBucket: "COLE_AQUI",
-  messagingSenderId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+  apiKey: "AIzaSyA6nN8Z6chNH7ITn_3JW_En9ycIgVkaHBc",
+    authDomain: "sistema-financeiro-dfc08.firebaseapp.com",
+    projectId: "sistema-financeiro-dfc08",
+    storageBucket: "sistema-financeiro-dfc08.firebasestorage.app",
+    messagingSenderId: "59510433298",
+    appId: "1:59510433298:web:23e66afa05fa7df3981acb",
+    measurementId: "G-CSD0EF2XH2"
 };
