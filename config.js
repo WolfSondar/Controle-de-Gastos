03237e38a4399164ef6b1acf2ac438e6601994f9
davi-souza-vmg <davi.ribeiro@viacaominasgerais.com.br>
2026-09-22 08:19:03 -0300

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-// Cole aqui a URL do seu Apps Script (a que termina em /exec).
-// Veja o passo a passo no arquivo Code.gs ou no README.md.
-const API_URL = "https://script.google.com/macros/s/AKfycbxgbGSwFX0DnM7GUf7uF4n2MxLsVXtH2obphoMn3YhYkQtoYEmZ0JkzV2bzT7-VSrConQ/exec";
